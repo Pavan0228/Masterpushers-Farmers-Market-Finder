a2e91cd251e50f30ee1b777c93168d2b5800c25d
sridhar1030 <sridharpillai75@gmail.com>
2025-03-08 10:55:53 +0530

    Otp verification done

--- backend/routers/courier.routes.js
+++ backend/routers/courier.routes.js
@@ -4,6 +4,7 @@ import {
     deleteCourier,
     getCourierById,
     getCouriers,
+    getCourierOrders,
     makeVerify,
     updateCourier,
 } from "../controller/courier.controller.js";
@@ -12,6 +13,7 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const courierRouter = express.Router();
 
 
+courierRouter.get("/orders", verifyJWT, getCourierOrders);
 courierRouter.get("/", getCouriers);
 
 courierRouter.get("/:id", getCourierById);
@@ -19,7 +21,6 @@ courierRouter.get("/:id", getCourierById);
 courierRouter.put("/:id", upload, updateCourier);
 
 courierRouter.patch("/:id/verify", verifyJWT, makeVerify);
-
 courierRouter.delete("/:id", deleteCourier);
 
 export { courierRouter };
